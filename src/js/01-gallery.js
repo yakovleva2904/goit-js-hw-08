6313bd436017c7d1d7e850a341560ed48689f0d8
yakovleva2904 <yakovlevaolga2904@gmail.com>
2022-02-20 15:38:10 +0200

    upd gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createImgCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+            </a>`;
+        })
+        .join('');
+} 
+const lightbox = new SimpleLightbox('.gallery a', { /* options */
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionType: 'attr',
+    captionPosition: 'bottom',
+
+});
