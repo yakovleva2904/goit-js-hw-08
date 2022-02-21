10f8900030967ea008442599e3bb8a8418144815
yakovleva2904 <yakovlevaolga2904@gmail.com>
2022-02-21 21:04:36 +0200

    Update 02-video.js
    videoplayer done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
+
+const lOCAL_TIME_SET = 'videoplayer-current-time';
+const localTime = localStorage.getItem(lOCAL_TIME_SET);    
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe); 
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+if (localTime) {
+    player.setCurrentTime(localTime);
+};
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.on('timeupdate', throttle(onTimeUpdate, 1000))
+
+function onTimeUpdate(e) { 
+    const time = e.seconds;
+    localStorage.setItem(lOCAL_TIME_SET, time);
+};
